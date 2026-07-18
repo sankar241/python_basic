cd38779b6fdf2b79ac91667c3592c270ac5e3b4f
sankar241 <stylishsankar72@gmail.com>
2026-07-18 21:49:24 +0530

    Add files via upload

---
+++ python_day2.py
@@ -0,0 +1,24 @@
+
+num = 'yash'
+print(type(num))
+num = 9
+print(type(num))
+num = [5,6]
+print(type(num))
+_num = 90
+num = 9
+Num = 99
+num_2 = 7
+print(_num, num, Num, num_2)
+num = 7
+num_2 = 77
+print(num == num_2)
+print(num != num_2)
+num = num_2 = num_3 = 77
+print(num, num_2, num_3)
+num, num_2 = 7, 77
+print(num, num_2)
+num = 7
+num_2 = 77
+num, num_2 = num_2, num
+print(num, num_2)
